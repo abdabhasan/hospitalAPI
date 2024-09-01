fb6d99d3daddd2f2a80b13037de592f9ccfe1766
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2024-09-01 18:16:46 +0300

    created GenerateToken function

---
+++ HospitalPresentation.API/Helpers/TokenHelper.cs
@@ -0,0 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using HospitalDataLayer.Infrastructure.DTOs.User;
+using Microsoft.IdentityModel.Tokens;
+
+namespace HospitalPresentation.API.Helpers
+{
+    public class TokenHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(UserDTO user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
