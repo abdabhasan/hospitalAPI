a024191a89fb06c737a237332e7e0deacaabe0e2
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2024-09-02 14:41:44 +0300

    managed roles for Billing

--- HospitalPresentation.API/Controllers/BillController.cs
+++ HospitalPresentation.API/Controllers/BillController.cs
@@ -1,11 +1,13 @@
 using HospitalBusinessLayer.Core;
 using HospitalDataLayer.Infrastructure.DTOs.Bill;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalPresentation.API.Controllers
 {
     [ApiController]
     [Route("api/bills")]
+    [Authorize(Policy = "BillingPolicy")]
     public class BillController : ControllerBase
     {
 
@@ -21,6 +23,8 @@ namespace HospitalPresentation.API.Controllers
         [HttpPost("CreateBill", Name = "CreateBill")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<int>> CreateBillAsync([FromBody] CreateBillDTO createBillDto)
         {
@@ -52,6 +56,9 @@ namespace HospitalPresentation.API.Controllers
 
         [HttpDelete("DeleteBill/{billId}", Name = "DeleteBill")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<bool>> DeleteBillAsync(int billId)
@@ -85,6 +92,9 @@ namespace HospitalPresentation.API.Controllers
 
         [HttpGet("GetAllBills", Name = "GetAllBills")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<BillDTO>>> GetAllBillsAsync()
@@ -111,6 +121,9 @@ namespace HospitalPresentation.API.Controllers
 
         [HttpGet("GetBillById", Name = "GetBillById")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<BillDTO>> GetBillByIdAsync(int BillId)
@@ -137,6 +150,9 @@ namespace HospitalPresentation.API.Controllers
 
         [HttpGet("GetBillsForPatientByPatientId", Name = "GetBillsForPatientByPatientId")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<BillDTO>>> GetBillsForPatientByPatientIdAsync(int patientId)
@@ -163,6 +179,9 @@ namespace HospitalPresentation.API.Controllers
 
         [HttpGet("GetBillsForPatientByPatientName", Name = "GetBillsForPatientByPatientName")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<BillDTO>>> GetBillsForPatientByPatientNameAsync(string patientName)
